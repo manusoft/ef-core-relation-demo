07a0fcbb73db5a14310d3fd3498f8ce1c5f906b9
manoj@office <alconsoft@gmail.com>
2024-12-16 09:25:44 +0400

    Setup connecton string and register.

---
+++ EFCoreRelation/Data/AppDbContext.cs
@@ -0,0 +1,9 @@
+﻿using EFCoreRelation.Controllers;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCoreRelation.Data;
+
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+{
+    
+}
--- EFCoreRelation/Program.cs
+++ EFCoreRelation/Program.cs
@@ -1,8 +1,21 @@
+using EFCoreRelation.Data;
+using Microsoft.EntityFrameworkCore;
+using Scalar.AspNetCore;
+using System.Text.Json;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ??
+    throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 
-builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connectionString));
+
+builder.Services.AddControllers().AddJsonOptions(options=>
+{
+    options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles;
+});
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
@@ -12,6 +25,7 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference();
 }
 
 app.UseHttpsRedirection();
